9252a3a12b023e5f056c3eced3a962ed948b0613
Yury Yarashevich <yura.yaroshevich@gmail.com>
2023-02-03 23:02:53 +0100

    Added example of snippets which are crashed in Release build when run on iOS

--- TaskCrasher/ViewController.swift
+++ TaskCrasher/ViewController.swift
@@ -7,13 +7,127 @@
 
 import UIKit
 
+func globalAsyncFunction() async {
+  Task { // crash with libgmalloc enabled at this line on iOS <= 15.5
+    
+  }
+}
+
+class MyClass {
+  func asyncFunction() async {
+    Task { // crash with libgmalloc enabled at this line on iOS <= 15.5
+      
+    }
+  }
+  
+  func syncFunction() {
+    Task {
+      
+    }
+  }
+}
+
+func runSendableClosure(_ closure: @Sendable @escaping () -> Void) {
+  closure()
+}
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    ok0()
+    ok1()
+    ok2()
+    ok3()
+    // BUG: Uncomment any to cause crash with libgmalloc enabled
+//    willCauseHeapCorruption1()
+//    willCauseHeapCorruption2()
+//    willCauseHeapCorruption3()
+//    willCauseHeapCorruption4()
+//    willCauseHeapCorruption5()
+    willCauseHeapCorruption6();
   }
 
+  func willCauseHeapCorruption1() {
+    Task {
+      await globalAsyncFunction()
+    }
+  }
+  
+  func willCauseHeapCorruption2() {
+    Task {
+      let c = MyClass()
+      await c.asyncFunction()
+    }
+  }
 
+  func willCauseHeapCorruption3() {
+    @globalActor
+    struct MyGlobalActor {
+      private actor ActorImpl { }
+
+      public static let shared: some Actor = ActorImpl()
+    }
+
+    Task { @MyGlobalActor in
+      Task { // crash with libgmalloc enabled at this line on iOS <= 15.5
+        
+      }
+    }
+  }
+  
+  func willCauseHeapCorruption4() {
+    runSendableClosure {
+      Task { // crash with libgmalloc enabled at this line on iOS <= 15.5
+      }
+    }
+  }
+  
+  func willCauseHeapCorruption5() {
+    Task.detached {
+      Task {
+        
+      }
+    }
+  }
+
+  func willCauseHeapCorruption6() {
+    Task {
+      let c = MyClass()
+      c.syncFunction()
+    }
+  }
+
+  func ok0() {
+    Task {
+      
+    }
+  }
+
+  func ok1() {
+    Task {
+      Task {
+        
+      }
+    }
+  }
+  
+  func ok2() {
+    DispatchQueue.global().async {
+      Task {
+        
+      }
+    }
+  }
+  
+  func ok3() {
+    DispatchQueue.global().async {
+      Task {
+        Task {
+          
+        }
+      }
+    }
+  }
 }
 
